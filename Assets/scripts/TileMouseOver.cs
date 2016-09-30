a89fa52e62bfadf34b3305553b90da404d182b87
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-30 11:11:04 +0100

    Toggle alternate colour on mouse hover

---
+++ Assets/scripts/TileMouseOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMouseOver : MonoBehaviour {
+
+	private Renderer ren;
+	private Color origCol;
+
+	private void Start () {
+		ren = GetComponent<Renderer> ();
+		origCol = ren.material.color;
+	}
+
+	private void OnMouseOver () {
+		ren.material.color = Color.cyan;
+	}
+
+	private void OnMouseExit () {
+		ren.material.color = origCol;
+	}
+}
