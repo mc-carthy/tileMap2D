1c1c9093f36f7d3e11b300209ce56357f42e380e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-30 13:29:37 +0100

    Build mesh procedurally building vertices, triangles, normals and UVs from supplied map and tile dimensions

--- Assets/scripts/TileMap.cs
+++ Assets/scripts/TileMap.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
@@ -9,6 +10,11 @@ public class TileMap : MonoBehaviour {
 	private MeshFilter mFil;
 	private MeshRenderer mRen;
 	private MeshCollider mCol;
+
+	// Number of tiles
+	private int size_x = 100;
+	private int size_z = 50;
+	private float tileSize = 1;
 
 	private void Start () {
 		mFil = GetComponent<MeshFilter> ();
@@ -20,34 +26,47 @@ public class TileMap : MonoBehaviour {
 
 	private void BuildMesh () {
 
+		int numTiles = size_x * size_z;
+		int vsize_x = size_x + 1;
+		int vsize_z = size_z + 1;
+		int numVertices = vsize_x * vsize_z;
+
+		int numTriangles = numTiles * 2;
+
 		// Generate mesh data
-		Vector3[] vertices = new Vector3[4];
-		int[] triangles = new int[6];
-		Vector3[] normals = new Vector3[4];
-		Vector2[] uv = new Vector2[4];
+		Vector3[] vertices = new Vector3[numVertices];
+		Vector3[] normals = new Vector3[numVertices];
+		Vector2[] uv = new Vector2[numVertices];
 
-		vertices [0] = new Vector3 (0, 0, 0);
-		vertices [1] = new Vector3 (1, 0, 0);
-		vertices [2] = new Vector3 (0, 0, -1);
-		vertices [3] = new Vector3 (1, 0, -1);
+		int[] triangles = new int[numTriangles * 3];
 
-		triangles [0] = 0;
-		triangles [1] = 3;
-		triangles [2] = 2;
+		int x, z;
 
-		triangles [3] = 0;
-		triangles [4] = 1;
-		triangles [5] = 3;
+		for (z = 0; z < vsize_z; z++) {
+			for (x = 0; x < vsize_x; x++) {
 
-		normals [0] = Vector3.up;
-		normals [1] = Vector3.up;
-		normals [2] = Vector3.up;
-		normals [3] = Vector3.up;
+				vertices [z * vsize_x + x] = new Vector3 (x * tileSize, 0, z * tileSize);
+				normals [z * vsize_x + x] = Vector3.up;
+				uv [z * vsize_x + x] = new Vector2 ((float)x / vsize_x, (float)z / vsize_z);
+			}
+		}
 
-		uv [0] = new Vector2 (0, 1);
-		uv [1] = new Vector2 (1, 1);
-		uv [2] = new Vector2 (0, 0);
-		uv [3] = new Vector2 (1, 0);
+		for (z = 0; z < size_z; z++) {
+			for (x = 0; x < size_x; x++) {
+
+				int squareIndex = z * size_x + x;
+				int triOffset = squareIndex * 6;
+
+				triangles[triOffset + 0] = z * vsize_x + x + 		   0;
+				triangles[triOffset + 1] = z * vsize_x + x + vsize_x + 0;
+				triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 1;
+
+				triangles[triOffset + 3] = z * vsize_x + x + 		   0;
+				triangles[triOffset + 4] = z * vsize_x + x + vsize_x + 1;
+				triangles[triOffset + 5] = z * vsize_x + x + 		   1;
+
+			}
+		}
 
 		// Create new mesh and populate it with the created data
 		Mesh mesh = new Mesh ();
