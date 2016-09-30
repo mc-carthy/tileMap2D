915f2de98b941028f89ad32d982ca434f5d3b5f7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-30 11:20:31 +0100

    Recreate previous functionality using raycasting instead, in order to look through obstacles between tiles and camera

--- Assets/scripts/TileMouseOver.cs
+++ Assets/scripts/TileMouseOver.cs
@@ -3,19 +3,29 @@ using System.Collections;
 
 public class TileMouseOver : MonoBehaviour {
 
+	private Camera mainCam;
+	private Collider col;
 	private Renderer ren;
 	private Color origCol;
+	[SerializeField]
+	private Color highlightCol = Color.cyan;
 
 	private void Start () {
 		ren = GetComponent<Renderer> ();
 		origCol = ren.material.color;
+		mainCam = Camera.main;
+		col = GetComponent<Collider> ();
 	}
 
-	private void OnMouseOver () {
-		ren.material.color = Color.cyan;
-	}
+	private void Update () {
 
-	private void OnMouseExit () {
-		ren.material.color = origCol;
+		Ray ray = mainCam.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (col.Raycast (ray, out hit, 100)) {
+			ren.material.color = highlightCol;
+		} else {
+			ren.material.color = origCol;
+		}
 	}
 }
