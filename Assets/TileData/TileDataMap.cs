cd3a2539239f7970244bc160bc1e68d9270778ce
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-02 17:19:24 +0100

    Create TileDataMap and TileDataTile classes in order to encapsulate tile data and separate logic and graphics

---
+++ Assets/TileData/TileDataMap.cs
@@ -0,0 +1,20 @@
+﻿public class TileDataMap {
+
+	private TileDataTile[] _tiles;
+	private int _width;
+	private int _height;
+
+	public TileDataMap(int width, int height) {
+		_width = width;
+		_height = height;
+
+		_tiles = new TileDataTile[_width * _height];
+	}
+
+	public TileDataTile GetTile (int x, int y) {
+		if (x < 0 || x >= _width || y < 0 || y >= _height) {
+			return null;
+		}
+		return _tiles [y * _width + x];
+	}
+}
---
+++ Assets/TileData/TileDataTile.cs
@@ -0,0 +1,10 @@
+﻿public class TileDataTile {
+
+	public const int TILE_OCEAN = 0;
+	public const int TILE_GRASS = 1;
+	public const int TILE_DESERT = 2;
+	public const int TILE_MOUNTAIN = 3;
+
+	public int type = TILE_OCEAN;
+
+}
